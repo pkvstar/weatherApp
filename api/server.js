0aa23a4dad13818b84f1136fdc545a013f73af9a
pkvstar <pk8649726@gmail.com>
2025-04-12 15:44:38 +0530

    done

--- api/server.js
+++ api/server.js
@@ -5,6 +5,9 @@ const cors = require('cors')
 app.use(cors());
 require('dotenv').config();
 app.use('/weather',weatherRoute);
+app.get('/',(req,res)=>{
+    res.send("hello world");
+})
 const port = process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`server is running on ${port} PORT`);
